980668a430a9635596b38543b62c5cc4153e8c9b
dhruvesh16 <dhruvesh3466@protonmail.com>
2022-12-11 13:30:30 +0530

    Spacewar: Switch to AOSP NFC stack

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,7 +93,6 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.nt
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/nxp/nfc/vendor_framework_compatibility_matrix.xml \
     vendor/xtended/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
