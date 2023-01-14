42d7cfa5cdf67967c1c1b11244a5ded21b611910
Kujou Yuko <i@779.moe>
2023-01-14 13:39:11 +0800

    Spacewar: Switch to QTI health HAL implementation
     * Import from LA.UM.9.14.r1-21000-LAHAINA.QSSI13.0.
     * Fixed the battery meter not working properly.
    
    Change-Id: Ie17b01135fdd311ca512da6997dc875894ea5458
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- device.mk
+++ device.mk
@@ -279,7 +279,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl-Spacewar \
     android.hardware.health@2.1-service
 
 # HIDL
