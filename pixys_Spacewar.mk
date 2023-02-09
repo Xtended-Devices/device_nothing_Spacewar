0eed82537a3f71a01ea2f1a87eeb64a2a98af7f1
aswin7469 <aswinas@pixysos.com>
2023-02-09 19:42:47 +0530

    Spacewar: pixys: specify we ship oem camera app
    * exclude aperture
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- pixys_Spacewar.mk
+++ pixys_Spacewar.mk
@@ -31,6 +31,7 @@ TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_OEM_CAMERA := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := pixys_Spacewar
